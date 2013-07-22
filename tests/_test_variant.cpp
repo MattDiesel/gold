5490e27ce5741159ffb415f1b4484be561698fd3
Matt Diesel <M@ttDiesel.co.uk>
2013-07-22 22:37:49 +0100

    _test_variant now demos use of array variant.

--- tests/_test_variant.cpp
+++ tests/_test_variant.cpp
@@ -1,10 +1,21 @@
 
 #include "../variant/variant.h"
+#include "../variant/vararray.h"
 #include <iostream>
 
 
+
+
 int main() {
-	gold::Variant v = 42;
+	gold::Variant v = new gold::VarArray( );
+
+	{
+		gold::VarArray* a = v.Raw<gold::VarArray>();
+		a->Add( 42 );
+		a->Add( "Hello" );
+		a->Add( "World" );
+		a->Add( true );
+	}
 
 	std::cout << v << std::endl;
 
